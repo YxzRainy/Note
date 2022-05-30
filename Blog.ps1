9de15ab7d623ac1da9bc349617e2bca52d13957d
YxzRainy <1971438937@qq.com>
2022-05-30 11:16:33 +0800

    new blog

--- Blog.ps1
+++ Blog.ps1
@@ -1,4 +1,4 @@
 ri -Recurse E:\Github\MyBlog\public\*
 hugo -D
-ri -Recurse E:\Github\MyWeb\Sub\Blog\*
-Copy-Item -Recurse E:\Github\MyBlog\public\* E:\Github\MyWeb\Sub\Blog
+# ri -Recurse E:\Github\MyWeb\Sub\Blog\*
+# Copy-Item -Recurse E:\Github\MyBlog\public\* E:\Github\MyWeb\Sub\Blog
---
+++ content/blog/Essay/Hello World.md
@@ -0,0 +1,16 @@
+---
+title: Hello World
+date: 2021-10-01 6:00:00
+categories:
+        - Essay
+tags:
+        - Blog
+---
+
+# Hello World
+
+2021 年 10 月 1 日，
+
+我的站点正式发布。
+
+愿你被世界温柔以待。
