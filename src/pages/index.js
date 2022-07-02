237e8ffdb820a6911de30ce930bcb6ef3ddc9f86
YxzRainy <1971438937@qq.com>
2022-07-02 16:57:54 +0800

    note

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
 					<Link className="button button--secondary button--lg" to="/docs/intro">
-						Reading Rainy's Notes
+						Reading Notes
 					</Link>
 				</div>
 			</div>
